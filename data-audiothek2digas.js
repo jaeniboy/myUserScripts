53f2a94fe382d27ba068fea84b4388b0ab2a5166
jaeniboy <31035718+jaeniboy@users.noreply.github.com>
2023-02-07 14:21:23 +0100

    Update data-audiothek2digas.js
    refactoring and simplifying source code using literal strings for templates and vanilla javascript instead of xpath as selctor

--- data-audiothek2digas.js
+++ data-audiothek2digas.js
@@ -7,43 +7,39 @@
 // @exclude       http://www.diveintogreasemonkey.org/*
 // ==/UserScript==
 
-var templates = [ // Change value based on download url
-  
+const templates = [ // Change value based on download url
+  {
+    "id": "Standard",
+    "selector:": "",
+    "template": function(title="",author="",description="",keywords="") { return `++0\n++1\nBETI: ${title}\nAUT: ${author}\nGATT:\nPTEX: ${description} (PR-Text)\nHD: ${keywords}`;}
+  },
   {
    	"id": "Der Gangster, der Junkie und die Hure", 
    	"selector": "https://avdlswr-a.akamaihd.net/swr3/podcasts-audioreihen/der-gangster-der-junkie-und-die-hure/",
-   	"template": function(data=""){return "SPR: Pollux, Maximilian (ehem. Krimineller)\nSPR: Lemke, Roman (ehem. Drogenabhängiger)\nSPR: Titan, Tara (ehem. Prostituierte)\n"}
+    "template": function(title="",author="",description="",keywords="") { return `++0\nRTI: Der Gangster, der Junkie und die Hure\n++1\nBETI: ${title}\nSPR: Pollux, Maximilian (ehem. Krimineller)\nSPR: Lemke, Roman (ehem. Drogenabhängiger)\nSPR: Titan, Tara (ehem. Prostituierte)\nGATT: Gespräch\nPTEX: ${description} (PR-Text)\nHD: ${keywords}`;}
   },
   {
   	"id": "SWR3 Topthema",
   	"selector": "https://avdlswr-a.akamaihd.net/swr3/podcasts-audioreihen/topthemen/",
-  	"template": function(data="") {return "AUT: " + data + "\nSPR: " + data + "\nOTG:\n"}
+    "template": function(title="",author="",description="",keywords="") {  return `++0\nRTI: SWR3 Topthema\n++1\nBETI: ${title}\nAUT: ${author}\nSPR: ${author}\nOTG:\nGATT: Bericht\nPTEX: ${description} (PR-Text) AM-geeignet\nHD: ${keywords}`;}
   },
   {
-    "id":"Fünf Minuten vor dem Tod",
-    "selector":"https://avdlswr-a.akamaihd.net/dasding/podcasts/5-minuten-vor-dem-tod-der-kriminalpodcast/",
-    "template": function(data="") {return "SPR: Bleich, Luisa\nSPR: Schmidt, Joost\n"}
+    "id":"Fünf Minuten vor dem Tod - der Kriminalpodcast",
+    "selector": "https://avdlswr-a.akamaihd.net/dasding/podcasts/5-minuten-vor-dem-tod-der-kriminalpodcast/",
+    "template": function(title="",author="",description="",keywords="") { return `++0\nRTI: Fünf Minuten vor dem Tod - der Kriminalpodcast\n++1\nBETI: ${title}\nSPR: Bleich, Luisa\nSPR: Schmidt, Joost\nGATT: Gespräch\nGSP:\nPTEX: ${description} (PR-Text)\nHD: ${keywords}`;}
   },
   {
-    "id":"",
-    "selector": "YOUR SELECTOR",
-    "template": function(data="") {return ""}
+    "id":"Korridore",
+    "selector": "https://avdlswr-a.akamaihd.net/swr/swr2/hoerspiel/korridore-mystery-horror-serie/",
+    "template": function(title="",author="",description="",keywords="") { return `++0\nRTI: Korridore - Mystery-Horror-Serie\n++1\nBETI: ${title}\nSPR: Schimmelpfennig, Max\nSPR: Arikan, Nisan\nSPR: Bähnk, Ulrich\nTAL: Lodi, Giacomo (Aufnahmen)\nTAL: Frerks, Thies (Aufnahmen)\nTAL: Güthaus, Thomas (Sounddesign und Mischung)\nAUT: Haase, Moritz (Ideengeber)\nAUT: Henriks, Lars\nREG: Henriks, Lars\nDRA: Maage, Mareike\nPTEX: ${description} (PR-Text)\nHD: ${keywords}`;}
+
   }
 ]
 
 
-function getElementByXpath(path) { // Xpath benutzen
-  return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-}
-
-
 function copyToClipboard(text) {
-    var dummy = document.createElement("textarea");
-    // to avoid breaking orgain page when copying more words
-    // cant copy when adding below this code
-    // dummy.style.display = 'none'
+    let dummy = document.createElement("textarea");
     document.body.appendChild(dummy);
-    //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
     dummy.value = text;
     dummy.select();
     document.execCommand("copy");
@@ -51,59 +47,40 @@ function copyToClipboard(text) {
 }
 
 
-function getMetadata() { // Metadaten abrufen
+function getData() { // Metadaten abrufen
   
-  var title = getElementByXpath("/html/body/div[1]/div[3]/table/tbody/tr[2]/td[2]/text()").data;
-  var author = getElementByXpath("/html/body/div[1]/div[3]/table/tbody/tr[4]/td[2]/text()").data;
-  var description = getElementByXpath("/html/body/div[1]/div[3]/table/tbody/tr[6]/td[2]/text()").data;
-  var keywords = getElementByXpath("/html/body/div[1]/div[3]/table/tbody/tr[5]/td[2]/text()").data;
+  let dataElements = document.getElementsByTagName("td")
   
+  let title = dataElements[1].innerText
+  let author = dataElements[5].innerText
+  let description = dataElements[9].innerText
+  let keywords = dataElements[7].innerText
+  let url = document.getElementById("response").getElementsByTagName("div")[0].innerText;
   
-  var metadata = [
-    title,
-    author,
-    description,
-    keywords
-  ]
-
-  return(metadata)
-  
-}
-
-function getFormedMetadata() { // Metadaten formatieren
- 
-  var rawmetadata = getMetadata()
-  var url = document.getElementById("response").getElementsByTagName("div")[0].innerText;
-  var template = "AUT: " + rawmetadata[1] + " \n"
+  // Standard-Template zuweisen
+  let templateData = templates[0].template(title,author,description,keywords)
   
   // wählen Template für Podcastreihe basierend auf der URL aus
  	for (var temp of templates){   
-    if (url.includes(temp.selector)){ template = temp.template(rawmetadata[1]);}  
+  	if (url.includes(temp.selector)){ templateData = temp.template(title,author,description,keywords);}  
   }
- 
   
-  var formedmetadata = "++0\n" +
-      "++1\n" +
-      "BETI: " + rawmetadata[0] + " \n" +
-      template +
-      "ABS: " + rawmetadata[2].replace(/\n\s*\n/g, '\n') + " (PR-Text)\n" + 
-      "HD: " + rawmetadata[3] 
-
-  return(formedmetadata)
+  return(templateData)
   
 }
 
-function submitMetadata() { // formatierte Metadaten ausgeben
-	
-  var response = getFormedMetadata()
+
+
+function submitData() { // formatierte Metadaten ausgeben
+  let response = getData()
   copyToClipboard(response)
   alert("Formatierte Metadaten in Zwischenablage")
 }
 
-var zNode       = document.createElement ('div');
+let zNode       = document.createElement ('div');
 zNode.innerHTML = '<button id="myButton" type="button">'
                 + 'Metadaten für Digas-Erfassung</button>'
                 ;
 zNode.setAttribute ('id', 'myContainer');
-zNode.addEventListener("click", submitMetadata, false);
+zNode.addEventListener("click", submitData, false);
 document.body.appendChild (zNode);

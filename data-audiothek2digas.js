6fdb39078eb4bd92e78ecd7494f553f81a79804f
jaeniboy <31035718+jaeniboy@users.noreply.github.com>
2023-08-07 07:44:42 +0200

    Update data-audiothek2digas.js
    Changed SPR to HOST

--- data-audiothek2digas.js
+++ data-audiothek2digas.js
@@ -14,19 +14,19 @@ const templates = [ // Change value based on download url
     "template": function(title="",author="",description="",keywords="") { return `++0\n++1\nBETI: ${title}\nAUT: ${author}\nGATT:\nPTEX: ${description} (PR-Text)\nHD: ${keywords}`;}
   },
   {
-   	"id": "Der Gangster, der Junkie und die Hure", 
+   	"id": "Der Gangster, der Junkie und die Hure",
    	"selector": "https://avdlswr-a.akamaihd.net/swr3/podcasts-audioreihen/der-gangster-der-junkie-und-die-hure/",
-    "template": function(title="",author="",description="",keywords="") { return `++0\nRTI: Der Gangster, der Junkie und die Hure\n++1\nBETI: ${title}\nSPR: Pollux, Maximilian (ehem. Krimineller)\nSPR: Lemke, Roman (ehem. Drogenabhängiger)\nSPR: Workhard, Nina (Domina)\nGATT: Gespräch\nPTEX: ${description} (PR-Text)\nHD: ${keywords}`;}
+    "template": function(title="",author="",description="",keywords="") { return `++0\nRTI: Der Gangster, der Junkie und die Hure\n++1\nBETI: ${title}\nHOST: Pollux, Maximilian (ehem. Krimineller)\nHOST: Lemke, Roman (ehem. Drogenabhängiger)\nHOST: Workhard, Nina (Domina)\nGATT: Gespräch\nPTEX: ${description} (PR-Text)\nHD: ${keywords}`;}
   },
   {
   	"id": "SWR3 Topthema",
   	"selector": "https://avdlswr-a.akamaihd.net/swr3/podcasts-audioreihen/topthemen/",
-    "template": function(title="",author="",description="",keywords="") {  return `++0\n++1\nBETI: ${title}\nAUT: ${author}\nSPR: ${author}\nOTG:\nGATT: Bericht\nPTEX: ${description} (PR-Text) AM-geeignet\nHD: ${keywords}`;}
+    "template": function(title="",author="",description="",keywords="") { return `++0\n++1\nBETI: ${title}\nAUT: ${author}\nSPR: ${author}\nOTG:\nGATT: Bericht\nPTEX: ${description} (PR-Text) AM-geeignet\nHD: ${keywords}`;}
   },
   {
     "id":"Fünf Minuten vor dem Tod - der Kriminalpodcast",
     "selector": "https://avdlswr-a.akamaihd.net/dasding/podcasts/5-minuten-vor-dem-tod-der-kriminalpodcast/",
-    "template": function(title="",author="",description="",keywords="") { return `++0\nRTI: Fünf Minuten vor dem Tod - der Kriminalpodcast\n++1\nBETI: ${title}\nSPR: Bleich, Luisa\nSPR: Schmidt, Joost\nGATT: Gespräch\nGSP:\nPTEX: ${description} (PR-Text)\nHD: ${keywords}`;}
+    "template": function(title="",author="",description="",keywords="") { return `++0\nRTI: Fünf Minuten vor dem Tod - der Kriminalpodcast\n++1\nBETI: ${title}\nHOST: Bleich, Luisa\nHOST: Schmidt, Joost\nGATT: Gespräch\nGSP:\nPTEX: ${description} (PR-Text)\nHD: ${keywords}`;}
   },
   {
     "id":"Korridore",
@@ -48,25 +48,25 @@ function copyToClipboard(text) {
 
 
 function getData() { // Metadaten abrufen
-  
+
   let dataElements = document.getElementsByTagName("td")
-  
+
   let title = dataElements[1].innerText
   let author = dataElements[5].innerText
   let description = dataElements[9].innerText
   let keywords = dataElements[7].innerText
   let url = document.getElementById("response").getElementsByTagName("div")[0].innerText;
-  
+
   // Standard-Template zuweisen
   let templateData = templates[0].template(title,author,description,keywords)
-  
+
   // wählen Template für Podcastreihe basierend auf der URL aus
- 	for (var temp of templates){   
-  	if (url.includes(temp.selector)){ templateData = temp.template(title,author,description,keywords);}  
+ 	for (var temp of templates){
+  	if (url.includes(temp.selector)){ templateData = temp.template(title,author,description,keywords);}
   }
-  
+
   return(templateData)
-  
+
 }
 
 
